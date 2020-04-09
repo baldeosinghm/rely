80578cb004a9188b153c23c3d0583bdd56a02f33
Matthew Baldeosingh <baldeosinghm@allegheny.edu>
2020-04-09 18:17:47 -0400

    Create and restructured crossValidation module to train on proper data

---
+++ flaskApp/src/crossValidation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use("TKagg")
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
+from sklearn.linear_model import LinearRegression
+
+def cvPredictPrice(stock, days):
+    # make data frame
+    df = pd.read_csv("csv/" + stock + ".csv", sep='\s*,\s*', header=0, encoding='ascii', engine='python')
+    # Store the "Low" in the x var, and the "High" in the y var
+    # We want to use the Low as the predictor, the independent variable
+    # Stock's High is the dependent variable, what we want to predict
+    X = df['Low'].values.reshape(-1,1)
+    y = df['High'].values.reshape(-1,1)
+    # Here we split the data: 70% for training, 30% for testing in test_size
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    # Here we train our algorithm, "regressor"
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    # Use sklearns "cross_val_predict" method to take the estimator and
+    # perform a lerning algorithm over three subsets of the original
+    # dataset; cv is the # of partitioned subsets
+    # The prediction functions trains on the X (predictors) and y (outcome)
+    kFold_prediction = cross_val_predict(regressor, X, y, cv=3)
+    # Return forecast from inputted # of days
+    return kFold_prediction[days]
