d74997f02f40ee6336714839ea4857d59cd96633
Matthew Baldeosingh <baldeosinghm@allegheny.edu>
2020-04-09 18:20:00 -0400

    Update url management and add classes for each algorithm's flask form

--- flaskApp/forms.py
+++ flaskApp/forms.py
@@ -25,3 +25,13 @@ class PredictForm(FlaskForm):
     stockTicker = SelectField('Stock Ticker', choices=[("TWTR", "Twitter"), ("VZ", "Verizon"), ("FB", "Facebook"), ("TSLA", "Tesla"), ("GOOGL", "Google")])
     daysToPredict = SelectField('Days', choices=[("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5")])
     submit = SubmitField("Submit")
+
+class PredictCvForm(FlaskForm):
+    cVstockTicker = SelectField('Stock Ticker', choices=[("TWTR", "Twitter"), ("VZ", "Verizon"), ("FB", "Facebook"), ("TSLA", "Tesla"), ("GOOGL", "Google")])
+    cVdaysToPredict = SelectField('Days', choices=[("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5")])
+    submit = SubmitField("Submit")
+
+class PredictSvmForm(FlaskForm):
+    svMstockTicker = SelectField('Stock Ticker', choices=[("TWTR", "Twitter"), ("VZ", "Verizon"), ("FB", "Facebook"), ("TSLA", "Tesla"), ("GOOGL", "Google")])
+    svMdaysToPredict = SelectField('Days', choices=[("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5")])
+    submit = SubmitField("Submit")
--- flaskApp/relyApp.py
+++ flaskApp/relyApp.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
-from forms import RegistrationForm, LoginForm, PredictForm
+from forms import RegistrationForm, LoginForm, PredictForm, PredictCvForm, PredictSvmForm
 from src.generate_csv import stock_csv
 from src.linear_regression import predictPrice
+from src.crossValidation import cvPredictPrice
+from src.support_vector_regression import vectorPredictPrice
 import sys
 
 app = Flask(__name__)
@@ -17,8 +19,8 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/predict", methods=['GET', 'POST'])
-def predict():
+@app.route("/linear", methods=['GET', 'POST'])
+def linear():
     form = PredictForm()
     if form.validate_on_submit():
         stock=request.form["stockTicker"]
@@ -26,10 +28,38 @@ def predict():
         stock_csv(stock)
         prediction=predictPrice(stock, days)
         if (days==1):
-            flash(str(stock) + "'s High in " + str(days) + " day will be: $" + str(prediction[0]) + ".")
+            flash("Linear Regression | " + str(stock) + "'s High in " + str(days) + " day will be: $" + str(prediction[0]) + ".")
         else:
-            flash(str(stock) + "'s High in " + str(days) + " days will be: $" + str(prediction[0]) + ".")
-    return render_template('predict.html', title='Predict', form=form)
+            flash("Linear Regression | " + str(stock) + "'s High in " + str(days) + " days will be: $" + str(prediction[0]) + ".")
+    return render_template('linear.html', title='Linear Regression', form=form)
+
+@app.route("/kfold", methods=['GET', 'POST'])
+def kfold():
+    form = PredictCvForm()
+    if form.validate_on_submit():
+        stock=request.form["cVstockTicker"]
+        days=int(request.form["cVdaysToPredict"])
+        stock_csv(stock)
+        predictionTwo=cvPredictPrice(stock, days)
+        if (days==1):
+            flash("K-Fold CV | " + str(stock) + "'s High in " + str(days) + " day will be: $" + str(predictionTwo[0]) + ".")
+        else:
+            flash("K-Fold CV | " + str(stock) + "'s High in " + str(days) + " days will be: $" + str(predictionTwo[0]) + ".")
+    return render_template('kfold.html', title='K-Fold CV', form=form)
+
+@app.route("/svm", methods=['GET', 'POST'])
+def svm():
+    form = PredictSvmForm()
+    if form.validate_on_submit():
+        stock=request.form["svMstockTicker"]
+        days=int(request.form["svMdaysToPredict"])
+        stock_csv(stock)
+        predictionThree=vectorPredictPrice(stock, days)
+        if (days==1):
+            flash("Support Vector Machine | " + str(stock) + "'s High in " + str(days) + " day will be: $" + str(predictionThree) + ".")
+        else:
+            flash("Support Vector Machine | " + str(stock) + "'s High in " + str(days) + " days will be: $" + str(predictionThree) + ".")
+    return render_template('svm.html', title='SVM', form=form)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
